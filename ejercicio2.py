1fc1085754b031099906860ccee218c8c37cab94
sebasgclement <sebasclement.SC@gmail.com>
2023-06-13 19:18:29 -0300

    Primer commit

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+#Input
+N1 = float(input("Ingrese la primera nota: "))
+N2 = float(input("Ingrese la segunda nota: "))
+N3 = float(input("Ingrese la tercera nota: "))
+
+#Proceso
+promedio = (N1+N2+N3)/3
+
+#Output
+print("El promedio de notas es: ", promedio)
